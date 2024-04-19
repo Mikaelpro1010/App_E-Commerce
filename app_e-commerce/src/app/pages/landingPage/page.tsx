01094b27a7be32c53c8c6a563469c5116c0fb3b5
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-04-19 09:16:55 -0300

    melhorando o layout dos cards

--- app_e-commerce/src/app/pages/landingPage/page.tsx
+++ app_e-commerce/src/app/pages/landingPage/page.tsx
@@ -5,7 +5,7 @@ import Card from "../../../app/components/Card";
 function landingPage(){
     return (
         <main className="container mx-auto px-4">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
                 <Card />
                 <Card />
                 <Card />
