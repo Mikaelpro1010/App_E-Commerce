c66583f23fc372967e24f4d7dc80d0b0b44bad7d
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-04-19 10:39:45 -0300

    criando componente de carousel de imagens

--- app_e-commerce/src/app/pages/landingPage/page.tsx
+++ app_e-commerce/src/app/pages/landingPage/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "../../../app/components/Navbar";
 import Card from "../../../app/components/Card";
+import CarouselCustomNavigation from "../../components/CarouselCustomNavigation";
 
 function landingPage(){
     return (
         <main className="container mx-auto px-4">
+            <CarouselCustomNavigation />
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
                 <Card />
                 <Card />
