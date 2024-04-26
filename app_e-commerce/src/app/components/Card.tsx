f85db02c1e337dcdf7f19703b9d173c9d80c4dcb
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-04-26 14:03:12 -0300

    criando componente de card do site

--- app_e-commerce/src/app/components/Card.tsx
+++ app_e-commerce/src/app/components/Card.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, preco}) => {
   return (
     <div className="card bg-gray rounded-lg shadow-md hover:shadow-xl transition duration-300 cursor-pointer">
         <img className="card--image mt-4 rounded-lg" src="/Ecommerce.png" />
-      <h2 className="card--name text-2xl font-bold text-gray-800 mt-2 p-3">Tijolo</h2>
-      <span className="card--details text-gray-500 p-3">$20,00</span>
+      <h3 className="card--name text-2xl font-bold text-gray-800 mt-2 p-3">{name}</h3>
+      <span className="card--details text-gray-500 p-3">{preco}</span>
     </div>
   );
 };
